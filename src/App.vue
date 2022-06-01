fb1b8cdb193098ff5170c46018af4bc0260f7bc0
ACSEID1 <61818481+pondsecret@users.noreply.github.com>
2022-06-01 15:06:16 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ import { Janus } from 'janus-gateway'
 
 // const JANUS_URL = 'http://127.0.0.1:8088/janus'
 // const JANUS_URL = 'http://34.87.84.21:8088/janus'
-const JANUS_URL = 'http://34.143.225.243:8088/janus'
+const JANUS_URL = 'https://34.143.225.243:8089/janus'
 
 
 
